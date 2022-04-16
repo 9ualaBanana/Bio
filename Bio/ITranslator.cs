5fc322eae7279af4b5d76df9c46c387243734f7b
GualaBanana <tem4rik@gmail.com>
2022-04-16 15:30:00 +0300

    Support translation from and to any type.

---
+++ Bio/ITranslator.cs
@@ -0,0 +1,16 @@
+﻿namespace Bio;
+
+/// <summary>
+/// Defines a means for custom conversion of a typed object to any other type.
+/// </summary>
+/// <typeparam name="From">The type to convert.</typeparam>
+/// <typeparam name="To">The type to conver to.</typeparam>
+public interface ITranslator<in From, out To>
+{
+    /// <summary>
+    /// Converts <paramref name="obj"/> to its <typeparamref name="To"/> representation.
+    /// </summary>
+    /// <param name="obj">The object to represent as <typeparamref name="To"/>.</param>
+    /// <returns>The representation of <paramref name="obj"/> as <typeparamref name="To"/>.</returns>
+    To Translate(From obj);
+}
---
+++ Bio/TranslationExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Bio;
+
+/// <summary>
+/// Defines generic extension methods for custom conversion of a typed object to any other type.
+/// </summary>
+public static class TranslationExtensions
+{
+    /// <summary>
+    /// Converts <paramref name="obj"/> of type <typeparamref name="From"/> to the type <typeparamref name="To"/>
+    /// by passing it to <paramref name="translator"/>.
+    /// </summary>
+    /// <typeparam name="From">The initial type of <paramref name="obj"/>.</typeparam>
+    /// <typeparam name="To">The resulting type of <paramref name="obj"/>.</typeparam>
+    /// <param name="obj">The object to be translated.</param>
+    /// <param name="translator">The function that will be used for translation.</param>
+    /// <returns>The representation of <paramref name="obj"/> after passing it through <paramref name="translator"/>.</returns>
+    public static To TranslateWith<From, To>(this From obj, Func<From, To> translator) => translator(obj);
+    /// <summary>
+    /// Converts <paramref name="obj"/> of type <typeparamref name="From"/> to the type <typeparamref name="To"/>
+    /// by passing it to <paramref name="translator"/>'s <see cref="ITranslator{From, To}.Translate(From)"/> method.
+    /// </summary>
+    /// <typeparam name="From">The initial type of <paramref name="obj"/>.</typeparam>
+    /// <typeparam name="To">The resulting type of <paramref name="obj"/>.</typeparam>
+    /// <param name="obj">The object to be translated.</param>
+    /// <param name="translator">The instance of <see cref="ITranslator{From, To}"/> to be used for translation.</param>
+    /// <returns>The representation of <paramref name="obj"/> after passing it through <paramref name="translator"/>.</returns>
+    public static To TranslateWith<From, To>(this From obj, ITranslator<From, To> translator) => translator.Translate(obj);
+}
